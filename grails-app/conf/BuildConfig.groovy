17c9c130b92a82c9a1498d5cd9570da787fd6f6d
Burt Beckwith <burt@burtbeckwith.com>
2011-04-20 12:39:34 -0400

    cleanup

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -3,8 +3,6 @@ grails.project.test.class.dir = 'target/test-classes'
 grails.project.test.reports.dir = 'target/test-reports'
 grails.project.docs.output.dir = 'docs' // for backwards-compatibility, the docs are checked into gh-pages branch
 
-grails.plugin.location.'cloud-foundry' = '../grails-cloud-foundry'
-
 grails.project.dependency.resolution = {
 
 	inherits 'global'
